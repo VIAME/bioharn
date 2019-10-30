aa1e902089ee8a139978bdcc98223715d1ba9c39
joncrall <jon.crall@kitware.com>
2019-10-30 15:02:01 -0400

    wip

---
+++ bioharn/detect_eval.py
@@ -0,0 +1,298 @@
+from os.path import exists
+import ndsampler
+import torch
+import kwimage
+import netharn as nh
+import numpy as np
+import ubelt as ub
+import scriptconfig as scfg
+import torch.utils.data as torch_data
+from bioharn import detect_predict
+
+
+class DetectEvaluateConfig(scfg.Config):
+    default = {
+        'deployed': scfg.Value(
+            '/home/joncrall/work/bioharn/fit/runs/bioharn-det-v11-test-cascade/myovdqvi/deploy_MM_CascadeRCNN_myovdqvi_035_MVKVVR.zip',
+            help='deployed network filepath'),
+
+        'batch_size': scfg.Value(1, help=(
+            'number of images that run through the network at a time')),
+
+        'input_dims': scfg.Value('native', help=(
+            'size of input chip; or "native" which uses input_dims used in training')),
+
+        'window_dims': scfg.Value('full', help=(
+            'size of sliding window chip; or "full" which uses the entire frame; '
+            'or "native", which uses window_dims specified in training')),
+
+        'overlap': scfg.Value(0.0, help='overlap of the sliding window'),
+
+        'workers': scfg.Value(4, help='num CPUs for data loading'),
+
+        'verbose': 1,
+
+        # Note: these dont work exactly correct due to mmdetection model
+        # differences
+        'nms_thresh': 0.4,
+        'conf_thresh': 0.001,
+
+        'xpu': scfg.Value('argv', help='a CUDA device or a CPU'),
+
+        'dataset': scfg.Value('~/data/noaa/Habcam_2015_g027250_a00102917_c0001_v2_vali.mscoco.json'),
+        'workdir': scfg.Path('~/work/bioharn', help='Dump all results in your workdir'),
+
+        'out_dpath': scfg.Path('./detect_eval_out/', help='folder to send the output'),
+    }
+
+
+def evaluate_models(**kw):
+    """
+    Evaluate multiple models using a config file or CLI.
+
+    Ignore:
+        from bioharn.detect_eval import *  # NOQA
+        kw = {}
+    """
+    import itertools as it
+    if 'config' in kw:
+        config_fpath = kw['config']
+        defaults = ub.dict_diff(kw, {'config'})
+        multi_config = DetectEvaluateConfig(data=config_fpath, default=defaults, cmdline=True)
+    else:
+        multi_config = DetectEvaluateConfig(default=kw, cmdline=True)
+    print('MultiConfig: {}'.format(ub.repr2(multi_config.asdict())))
+
+    # Look for specific items in the base config where multiple values are
+    # given. We will evaluate over all permutations of these values.
+    base_config = multi_config.asdict()
+
+    model_fpaths = base_config.pop('deployed')
+    if not ub.iterable(model_fpaths):
+        model_fpaths = [model_fpaths]
+    model_fpaths = [ub.expandpath(p) for p in model_fpaths]
+
+    for fpath in model_fpaths:
+        if not exists(fpath):
+            raise Exception('{} does not exist'.format(fpath))
+
+    search_space = {}
+    input_dims = base_config.pop('input_dims')
+    if ub.iterable(input_dims) and len(input_dims) and ub.iterable(input_dims[0], strok=True):
+        # multiple input dims to test are given as a list
+        search_space['input_dims'] = input_dims
+    else:
+        # only one input given, need to wrap
+        search_space['input_dims'] = [input_dims]
+
+    keys = list(search_space.keys())
+    basis = list(search_space.values())
+    config_perms = [ub.dzip(keys, permval) for permval in it.product(*basis)]
+
+    sampler = None
+    metric_fpaths = []
+    for model_fpath in ub.ProgIter(model_fpaths, desc='test model', verbose=3):
+        print('model_fpath = {!r}'.format(model_fpath))
+
+        predictor = None
+        for perm in ub.ProgIter(config_perms, desc='test config', verbose=3):
+
+            # Create the config for this detection permutation
+            config = ub.dict_union(base_config, perm)
+            config['deployed'] = model_fpath
+
+            if config['input_dims'] == 'native':
+                # hack, this info exists, but not in an easy form
+                deployed = nh.export.DeployedModel.coerce(config['deployed'])
+                train_config = eval(deployed.train_info()['extra']['config'], {})
+                config['input_dims'] = train_config['input_dims']
+
+            print('config = {}'.format(ub.repr2(config)))
+            self = DetectEvaluator(config)
+
+            # Reuse the dataset / predictor when possible
+            self.sampler = sampler
+            self.predictor = predictor
+            print('_init')
+            self._init()
+            print('evaluate')
+            metrics_fpath = self.evaluate()
+            print('metrics_fpath = {!r}'.format(metrics_fpath))
+            metric_fpaths.append(metrics_fpath)
+
+            # Save loaded predictor/sampler for the next run of this model/dataset
+            predictor = self.predictor
+            sampler = self.sampler
+
+
+class DetectEvaluator(object):
+    """
+    Ignore:
+        from bioharn.detect_eval import *  # NOQA
+        config = {'xpu': 0, 'batch_size': 1}
+        config['deployed'] = '/home/joncrall/work/bioharn/fit/runs/bioharn-det-v11-test-cascade/ovphtcvk/torch_snapshots/_epoch_00000030.pt'
+        config['deployed'] = '/home/joncrall/work/bioharn/fit/nice/bioharn-det-v10-test-retinanet/deploy_MM_RetinaNet_daodqsmy_010_QRNNNW.zip'
+        config = {'xpu': 0, 'batch_size': 4}
+        config['deployed'] = '/home/joncrall/work/bioharn/fit/nice/bioharn-det-v11-test-cascade/deploy_MM_CascadeRCNN_ovphtcvk_037_HZUJKO.zip'
+        self = DetectEvaluator(config)
+        self._init()
+        self.evaluate()
+    """
+
+    def __init__(self, config=None):
+        self.config = DetectEvaluateConfig(config)
+        self.predictor = None
+        self.sampler = None
+
+    def _ensure_sampler(self):
+        if self.sampler is None:
+            print('loading dataset')
+            coco_dset = ndsampler.CocoDataset(ub.expandpath(self.config['dataset']))
+            print('loaded dataset')
+            workdir = ub.expandpath(self.config['workdir'])
+            sampler = ndsampler.CocoSampler(coco_dset, workdir=workdir,
+                                            backend=None)
+            self.sampler = sampler
+            # self.sampler.frames.prepare(workers=min(2, self.config['workers']))
+            print('prepare frames')
+            self.sampler.frames.prepare(workers=self.config['workers'])
+            print('finished dataset load')
+
+    def _init(self):
+        self._ensure_sampler()
+
+        # Load model
+        deployed = nh.export.DeployedModel.coerce(self.config['deployed'])
+        nice = deployed.train_info()['nice']
+        if deployed.path is None:
+            model_tag = nice + '_' + ub.augpath(deployed._info['snap_fpath'], dpath='', ext='', multidot=True)
+        else:
+            model_tag = nice + '_' + ub.augpath(deployed.path, dpath='', ext='', multidot=True)
+
+        self.model_tag = model_tag
+        self.dset_tag = self.sampler.dset.tag.rstrip('.json')
+
+        # Load the trained model
+        pred_keys = set(detect_predict.DetectPredictConfig.default.keys()) - {'verbose'}
+        pred_cfg = ub.dict_subset(self.config, pred_keys)
+
+        if self.config['input_dims'] == 'native':
+            # hack, this info exists, but not in an easy form
+            train_config = eval(deployed.train_info()['extra']['config'], {})
+            pred_cfg['input_dims'] = train_config['input_dims']
+
+        if self.predictor is None:
+            # Only create the predictor if needed
+            print('Needs initial init')
+            self.predictor = detect_predict.DetectPredictor(pred_cfg)
+            self.predictor._ensure_model()
+        else:
+            # Reuse loaded predictors from other evaluators.
+            # Update the config in this case
+            needs_reinit = self.predictor.config['deployed'] != pred_cfg['deployed']
+            self.predictor.config.update(pred_cfg)
+            print('needs_reinit = {!r}'.format(needs_reinit))
+            if needs_reinit:
+                self.predictor._ensure_model()
+            else:
+                print('reusing loaded model')
+
+        self.classes = self.predictor.raw_model.classes
+
+        # The parameters that influence the predictions
+        pred_params = ub.dict_subset(self.predictor.config, [
+            'input_dims',
+            'window_dims',
+        ])
+        self.pred_cfg = nh.util.make_short_idstr(pred_params)
+        self.predcfg_tag = self.pred_cfg
+
+        self.paths = {}
+        out_dpath = ub.ensuredir(self.config['out_dpath'])
+
+        self.paths['base'] = ub.ensuredir((out_dpath, self.dset_tag, self.model_tag, self.pred_cfg))
+        self.paths['probs'] = ub.ensuredir((self.paths['base'], 'probs'))
+        self.paths['preds'] = ub.ensuredir((self.paths['base'], 'preds'))
+        self.paths['metrics'] = ub.ensuredir((self.paths['base'], 'metrics'))
+        self.paths['viz'] = ub.ensuredir((self.paths['base'], 'viz'))
+
+    def evaluate(self):
+        # TODO
+        gid_to_pred = self._run_predictions()
+
+        sampler = self.sampler
+
+        # Build true dets
+        classes = self.predictor.coder.classes
+
+        gid_to_truth = {}
+        for gid in gid_to_pred.keys():
+            annots = sampler.load_annotations(gid)
+            true_cids = [a['category_id'] for a in annots]
+            true_cidx = np.array([classes.id_to_idx[c] for c in true_cids])
+            true_dets = kwimage.Detections(
+                boxes=kwimage.Boxes([a['bbox'] for a in annots], 'xywh'),
+                class_idxs=true_cidx,
+                classes=classes,
+                # weights=np.ones(len(true_cidx)),
+            ).numpy()
+            gid_to_truth[gid] = true_dets
+
+        from netharn.metrics import DetectionMetrics
+        dmet = DetectionMetrics(classes=classes)
+        for gid in ub.ProgIter(list(gid_to_pred.keys())):
+            pred_dets = gid_to_pred[gid]
+            true_dets = gid_to_truth[gid]
+            dmet.add_predictions(pred_dets, gid=gid)
+            dmet.add_truth(true_dets, gid=gid)
+
+        cfsn_vecs = dmet.confusion_vectors()
+
+        print(dmet.score_coco(verbose=1))
+        print(dmet.score_voc())
+        dmet.score_netharn()
+
+        print('self.predcfg_tag = {!r}'.format(self.predcfg_tag))
+
+        # CascadeRCNN 512
+        # 'voc_mAP':  0.8379016325674102,
+        # 'coco_mAP': 0.8357345630911905
+        # netharn_AP: 0.8218177807000042,
+
+        # CascadeRCNN 1024
+        # coco_mAP: 0.8661484309698254
+        # voc_mAP: 0.8713472908407934
+        # netharn_AP: 0.8175519079596175
+
+        # CascadeRCNN 1024 - epoch 37
+        # coco_mAP: 0.8665642753052862,
+        # voc_mAP: 0.8712469728145056,
+        # netharn_AP: 0.8172452008810529,
+
+        # RetinaNet
+        # voc_mAP: 0.8418748847551925,
+        # coco_mAP: 0.8370107889677438
+
+        # TODO: cache detections to a file on disk.
+        # Give the DetectionMetrics code an entry point that just takes two
+        # coco files and scores them.
+
+        # import json
+        # metrics = {
+        #     'dset_tag': self.dset_tag,
+        #     'model_tag': self.model_tag,
+        #     'predcfg_tag': self.predcfg_tag,
+        #     'coco_score': coco_score,
+        #     'voc_score': voc_score,
+        #     'netharn_score': netharn_score,
+        # }
+        # metrics_fpath = join(self.paths['metrics'], 'metrics.json')
+        # with open(metrics_fpath, 'w') as file:
+        #     json.dump(nh.hyperparams._ensure_json_serializable(metrics), file)
+
+    def _run_predictions(self, only_draw=False):
+        self.predictor.config['verbose'] = 0
+
+        sampler = self.sampler
+        gid_to_pred = self.predictor.predict_sampler(sampler)
+        return gid_to_pred
