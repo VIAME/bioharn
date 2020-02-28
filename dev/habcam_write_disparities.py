eb457ccc9f4259e5f9bf476e911a9b16abd688b9
joncrall <erotemic@gmail.com>
2020-02-27 19:29:21 -0500

    wip

---
+++ dev/habcam_write_disparities.py
@@ -0,0 +1,50 @@
+import numpy as np
+from os.path import dirname
+from os.path import exists
+from os.path import join
+import ubelt as ub
+
+
+def main():
+    import ndsampler
+    from ndsampler.utils import util_futures
+
+    dset = ndsampler.CocoDataset(ub.expandpath('~/data/noaa/Habcam_2015_g027250_a00102917_c0001_v2_test.mscoco.json'))
+
+    jobs = util_futures.JobPool(mode='thread', max_workers=0)
+
+    for gid, img in ub.ProgIter(list(dset.imgs.items())):
+        if img.get('source', '') in ['habcam_2015_stereo', 'habcam_stereo']:
+            job = jobs.submit(_ensure_habcam_disparity_frame, dset, gid)
+            job.gid = gid
+
+    for job in ub.ProgIter(jobs, desc='collect results'):
+        gid = job.gid
+        job.result()
+
+
+def _ensure_habcam_disparity_frame(dset, gid):
+    from bioharn.detect_dataset import multipass_disparity
+    import kwimage
+
+    img = dset.imgs[gid]
+    image_fname = img['file_name']
+
+    disp_dpath = ub.ensuredir((dset.img_root, 'disparities'))
+    disp_fname = ub.augpath(image_fname, suffix='_left_disp_v7', ext='.cog.tif')
+    disp_fpath = join(disp_dpath, disp_fname)
+    ub.ensuredir(dirname(disp_fpath))
+
+    if not exists(disp_fpath):
+        # Note: probably should be atomic
+        img3 = dset.load_image(gid)
+        imgL = img3[:, 0:img3.shape[1] // 2]
+        imgR = img3[:, img3.shape[1] // 2:]
+        img_disparity = multipass_disparity(
+            imgL, imgR, scale=0.5, as01=True)
+        img_disparity = img_disparity.astype(np.float32)
+
+        kwimage.imwrite(disp_fpath, img_disparity, backend='gdal',
+                        compress='DEFLATE')
+
+    return disp_fpath
