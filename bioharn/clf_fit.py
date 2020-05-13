aef598ec3e93391e4f5376ce8d32e77f6c763210
joncrall <erotemic@gmail.com>
2020-05-13 12:44:48 -0400

    wip

--- bioharn/clf_fit.py
+++ bioharn/clf_fit.py
@@ -70,7 +70,7 @@ class ClfConfig(scfg.Config):
         # preference
         'num_draw': scfg.Value(4, help='Number of initial batchs to draw per epoch'),
         'draw_interval': scfg.Value(1, help='Minutes to wait between drawing'),
-        'draw_per_batch': scfg.Value(8, help='Number of items to draw within each batch'),
+        'draw_per_batch': scfg.Value(32, help='Number of items to draw within each batch'),
     }
 
     def normalize(self):
@@ -676,5 +676,48 @@ if __name__ == '__main__':
             --batch_size=32 \
             --num_batches=2000 \
             --balance=classes
+
+
+
+        python -m bioharn.clf_fit \
+            --name=bioharn-clf-rgb-v002 \
+            --train_dataset=$HOME/data/noaa_habcam/combos/habcam_cfarm_v8_train.mscoco.json \
+            --vali_dataset=$HOME/data/noaa_habcam/combos/habcam_cfarm_v8_vali.mscoco.json \
+            --schedule=ReduceLROnPlateau-p5-c5 \
+            --max_epoch=400 \
+            --augment=complex \
+            --init=noop \
+            --workdir=$HOME/work/bioharn \
+            --arch=resnext101 \
+            --channels="rgb" \
+            --optim=sgd \
+            --lr=1e-3 \
+            --input_dims=256,256 \
+            --normalize_inputs=True \
+            --backbone_init=$HOME/.cache/torch/checkpoints/resnext101_32x8d-8ba56ff5.pth \
+            --workers=8 \
+            --xpu=auto \
+            --batch_size=32 \
+            --balance=None
+
+        python -m bioharn.clf_fit \
+            --name=bioharn-clf-rgb-v003 \
+            --train_dataset=$HOME/data/noaa_habcam/combos/habcam_cfarm_v8_train.mscoco.json \
+            --vali_dataset=$HOME/data/noaa_habcam/combos/habcam_cfarm_v8_vali.mscoco.json \
+            --schedule=ReduceLROnPlateau-p5-c5 \
+            --max_epoch=400 \
+            --augment=simple \
+            --pretrained=/home/joncrall/work/bioharn/fit/runs/bioharn-clf-rgb-v001/nrorbmcb/deploy_ClfModel_nrorbmcb_051_UFCIUU.zip \
+            --workdir=$HOME/work/bioharn \
+            --arch=resnext101 \
+            --channels="rgb" \
+            --optim=sgd \
+            --lr=1e-3 \
+            --input_dims=256,256 \
+            --normalize_inputs=True \
+            --workers=8 \
+            --xpu=auto \
+            --batch_size=32 \
+            --balance=None
     """
     main()
