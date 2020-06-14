6aca6e7ace58ce0a76e654d7174829a583bc36bd
joncrall <erotemic@gmail.com>
2020-06-14 14:19:10 -0400

    wip

--- bioharn/models/efficientdet.py
+++ bioharn/models/efficientdet.py
@@ -469,7 +469,7 @@ class FocalLoss(nn.Module):
             bbox_annotation = annotations[j, :, :]
             bbox_annotation = bbox_annotation[bbox_annotation[:, 4] != -1]
 
-            if bbox_annotation.shape[0] == 0:
+            if bbox_annotation.view(-1).shape[0] == 0:
                 regression_losses.append(torch.tensor(0).float().to(device))
                 classification_losses.append(torch.tensor(0).float().to(device))
 
