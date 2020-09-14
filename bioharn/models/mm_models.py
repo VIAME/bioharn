c5c32eca78df3a64ba51be7dfc8d099e8f642682
joncrall <jon.crall@kitware.com>
2020-09-14 15:26:05 -0400

    Fix iou_thr warning

--- bioharn/models/mm_models.py
+++ bioharn/models/mm_models.py
@@ -1055,7 +1055,7 @@ class MM_MaskRCNN(MM_Detector):
                 min_bbox_size=0),
             rcnn=dict(
                 score_thr=0.05,
-                nms=dict(type='nms', iou_thr=0.5),
+                nms=dict(type='nms', iou_threshold=0.5),
                 max_per_img=100,
                 mask_thr_binary=0.5))
 
@@ -1421,7 +1421,7 @@ class MM_CascadeRCNN(MM_Detector):
                 min_bbox_size=0),
             rcnn=dict(
                 score_thr=0.05,
-                nms=dict(type='nms', iou_thr=0.5),
+                nms=dict(type='nms', iou_threshold=0.5),
                 max_per_img=100,
                 mask_thr_binary=0.5),
             keep_all_stages=False)
