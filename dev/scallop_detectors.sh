ec46be3bc9236ef9de8ddc0602df93c9a444cd14
joncrall <jon.crall@kitware.com>
2020-10-16 17:16:29 -0400

    wip

---
+++ dev/scallop_detectors.sh
@@ -0,0 +1,52 @@
+
+python -m bioharn.detect_fit \
+    --nice=bioharn-det-hrmask18-rgb-coi-v1 \
+    --workdir=$HOME/work/bioharn \
+    --train_dataset=$HOME/data/noaa_habcam/combos/habcam_cfarm_v8_train.mscoco.json \
+    --vali_dataset=$HOME/data/noaa_habcam/combos/habcam_cfarm_v8_vali.mscoco.json \
+    "--classes_of_interest=live sea scallop,swimming sea scallop,flatfish,clapper" \
+    --channels="rgb" \
+    --window_dims=768,768 \
+    --input_dims=window \
+    --window_overlap=0.5 \
+    --arch=MM_HRNetV2_w18_MaskRCNN \
+    --schedule=ReduceLROnPlateau-p5-c5 \
+    --max_epoch=400 \
+    --augment=complex \
+    --init=noop \
+    --optim=AdaBelief \
+    --lr=1e-3 \
+    --multiscale=False \
+    --normalize_inputs=imagenet \
+    --backbone_init=url \
+    --workers=8 \
+    --xpu=auto \
+    --batch_size=4 \
+    --num_batches=2000 \
+    --balance=None \
+    --bstep=8
+
+
+python -m bioharn.detect_fit \
+    --nice=validate_demo \
+    --workdir=$HOME/work/bioharn \
+    --train_dataset=special:shapes32 \
+    --vali_dataset=special:shapes32 \
+    --channels="rgb" \
+    --window_dims=768,768 \
+    --input_dims=window \
+    --window_overlap=0.5 \
+    --arch=MM_HRNetV2_w18_MaskRCNN \
+    --schedule=ReduceLROnPlateau-p5-c5 \
+    --max_epoch=400 \
+    --augment=complex \
+    --init=noop \
+    --optim=AdaBelief \
+    --lr=1e-3 \
+    --multiscale=False \
+    --normalize_inputs=imagenet \
+    --backbone_init=url \
+    --workers=8 \
+    --xpu=auto \
+    --batch_size=4 \
+    --bstep=8
