42ecf11253dddace03be5001def2205b90ba2fcd
joncrall <jon.crall@kitware.com>
2020-10-19 19:05:11 -0400

    wip

---
+++ dev/coco_cli/coco_add_dummy_segmentations.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import ubelt as ub
+import scriptconfig as scfg
+
+
+class CocoDummyAddSegmentationsCLI:
+    name = 'DummyAddSegmentations'
+
+    class CLIConfig(scfg.Config):
+        """
+        Compute summary statistics about a COCO dataset
+        """
+        default = {
+            'src': scfg.Value(['special:shapes8'], nargs='+', help='path to dataset'),
+
+            'dst': scfg.Value(None, help=(
+                'Save the dataset to a new file')),
+
+            'embed': scfg.Value(False, help='embed into interactive shell'),
+        }
+
+    @classmethod
+    def main(cls, cmdline=True, **kw):
+        import kwcoco
+        config = cls.CLIConfig(kw, cmdline=cmdline)
+        print('config = {}'.format(ub.repr2(dict(config), nl=1)))
+
+        if config['src'] is None:
+            raise Exception('must specify source: {}'.format(config['src']))
+
+        if isinstance(config['src'], str):
+            fpaths = [config['src']]
+        else:
+            fpaths = config['src']
+
+        datasets = []
+        for fpath in ub.ProgIter(fpaths, desc='reading datasets', verbose=1):
+            print('reading fpath = {!r}'.format(fpath))
+            dset = kwcoco.CocoDataset.coerce(fpath)
+            datasets.append(dset)
+
+        if config['embed']:
+            # Hidden hack
+            import xdev
+            xdev.embed()
+
+        # Hacks
+        add_sseg_to_cats = [
+            'live sea scallop',
+            'swimming sea scallop',
+            'dead sea scallop',
+        ]
+
+        for dset in datasets:
+            import kwimage
+            for ann in ub.ProgIter(dset.anns.values(), desc='add sseg'):
+                catname = dset._resolve_to_cat(ann['category_id'])['name']
+                if catname in add_sseg_to_cats:
+                    bbox = kwimage.Boxes([ann['bbox']], 'xywh').to_cxywh()
+                    xy = bbox.xy_center[0]
+                    w = bbox.width[0, 0]
+                    r = w / 2.0
+                    circle = kwimage.Polygon.circle(xy, r, resolution=32)
+                    ann['segmentation'] = circle.to_coco(style='new')
+
+        dset.fpath = config['dst']
+        print('dump dset.fpath = {!r}'.format(dset.fpath))
+        dset.dump(dset.fpath, newlines=True)
+
+
+_CLI = CocoDummyAddSegmentationsCLI
+
+if __name__ == '__main__':
+    _CLI._main()
+
--- dev/scallop_detectors.sh
+++ dev/scallop_detectors.sh
@@ -1,3 +1,36 @@
+python -m bioharn.detect_fit \
+    --nice=validate_demo \
+    --workdir=$HOME/work/bioharn \
+    --train_dataset=special:shapes32 \
+    --vali_dataset=special:shapes32 \
+    --channels="rgb" \
+    --window_dims=768,768 \
+    --input_dims=window \
+    --window_overlap=0.5 \
+    --arch=MM_HRNetV2_w18_MaskRCNN \
+    --schedule=ReduceLROnPlateau-p5-c5 \
+    --max_epoch=400 \
+    --augment=complex \
+    --init=noop \
+    --optim=AdaBelief \
+    --lr=1e-3 \
+    --multiscale=False \
+    --normalize_inputs=imagenet \
+    --backbone_init=url \
+    --workers=8 \
+    --xpu=auto \
+    --batch_size=4 \
+    --bstep=8
+
+
+python ~/code/bioharn/dev/coco_cli/coco_add_dummy_segmentations.py
+    --src $HOME/data/noaa_habcam/combos/habcam_cfarm_v8_vali.mscoco.json
+    --dst $HOME/data/noaa_habcam/combos/habcam_cfarm_v8_vali_dummy_sseg.mscoco.json
+
+python ~/code/bioharn/dev/coco_cli/coco_add_dummy_segmentations.py
+    --src $HOME/data/noaa_habcam/combos/habcam_cfarm_v8_train.mscoco.json
+    --dst $HOME/data/noaa_habcam/combos/habcam_cfarm_v8_train_dummy_sseg.mscoco.json
+
 
 python -m bioharn.detect_fit \
     --nice=bioharn-det-hrmask18-rgb-coi-v1 \
@@ -24,29 +57,4 @@ python -m bioharn.detect_fit \
     --batch_size=4 \
     --num_batches=2000 \
     --balance=None \
-    --bstep=8
-
-
-python -m bioharn.detect_fit \
-    --nice=validate_demo \
-    --workdir=$HOME/work/bioharn \
-    --train_dataset=special:shapes32 \
-    --vali_dataset=special:shapes32 \
-    --channels="rgb" \
-    --window_dims=768,768 \
-    --input_dims=window \
-    --window_overlap=0.5 \
-    --arch=MM_HRNetV2_w18_MaskRCNN \
-    --schedule=ReduceLROnPlateau-p5-c5 \
-    --max_epoch=400 \
-    --augment=complex \
-    --init=noop \
-    --optim=AdaBelief \
-    --lr=1e-3 \
-    --multiscale=False \
-    --normalize_inputs=imagenet \
-    --backbone_init=url \
-    --workers=8 \
-    --xpu=auto \
-    --batch_size=4 \
     --bstep=8
